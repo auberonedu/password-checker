696abe8625664479fc963c4a71d2329e30a7055c
Alexander Ruban <alexruban29@gmail.com>
2024-10-08 14:58:51 -0700

    added first JUnit test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,18 +1,33 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
     void testDescribePasswordLength() {
-
+        // Arrange
+        PasswordChecker password = new PasswordChecker(5, 10);
+        // Act
+        String actualLength = password.describePasswordLength("batman");
+        // Assert
+        assertEquals("medium", actualLength);
     }
 
     @Test
     void testIsAlphanumeric() {
+        // Arrange
 
+        // Act
+
+        // Assert
     }
 
     @Test
     void testIsBannedPassword() {
+        // Arrange
 
+        // Act
+
+        // Assert
     }
 }
