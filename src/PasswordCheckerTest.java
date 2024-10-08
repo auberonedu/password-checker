01ee42e16c8fec5fc89663c732c32ba81c36e921
Maxwell Maslov <maslov.maxwell@students.greenriver.edu>
2024-10-08 14:52:32 -0700

    Added test for short

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,9 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
   @Test
-  void testDescribePasswordLength() {
-
+  void testDescribePasswordLengthShort() {
+    // Arrange
+    PasswordChecker shortPassword = new PasswordChecker(6, 12);
+    // Act
+    String actualLength = shortPassword.describePasswordLength("abcde");
+    // Assert
+    assertEquals("short", actualLength);
   }
 
   @Test
