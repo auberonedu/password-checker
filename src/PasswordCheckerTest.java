7ea76dd82976cfd242282003606eee9178d21a3b
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2024-10-08 15:01:22 -0700

    added LengthTooShort test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,8 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
-    void testDescribePasswordLength() {
+    void testDescribePasswordLengthTooShort() {
+        //Arrange
+        PasswordChecker length = new PasswordChecker(6, 12);
+
+        //Act
+        String actual = length.describePasswordLength("a");
+
+        //Assert
+        assertEquals("short", actual);
 
     }
 
