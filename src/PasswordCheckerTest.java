c6a36faa1099bf17fc9a097e0931e04c62c3c012
Stefanurkal <stephenfranada@gmail.com>
2024-10-08 14:56:48 -0700

    add 3 test

---
+++ src/PasswordCheckerTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+public class PasswordCheckerTest {
+    @Test
+    void testDescribePasswordLengthShort() {
+
+    }
+    @Test
+    void testDescribePasswordLengthMedium() {
+
+    }
+
+    @Test
+    void testDescribePasswordLengthLong() {
+
+    }
+
+    @Test
+    void testIsAlphanumeric() {
+
+    }
+
+    @Test
+    void testIsBannedPassword() {
+
+    }
+}
