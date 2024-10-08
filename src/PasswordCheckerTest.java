22f88c151c0d34dd26dd12615c3d8b9aeb713e4b
Madeleine Chance <mechance782@gmail.com>
2024-10-08 15:01:30 -0700

    changed test name

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
-    void testDescribePasswordLength() {
+    void testDescribePasswordLengthMedium() {
         // Assign
         PasswordChecker checker = new PasswordChecker(4, 6);
         // Act
