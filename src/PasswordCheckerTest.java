566de3d14a9fd1809d4a27ca884e295e2ad43386
Felix Chen <felixchen@Felixs-MacBook-Pro-2.local>
2024-10-08 14:54:28 -0700

    added medium pw test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,9 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
   @Test
-  void testDescribePasswordLength() {
-
+  void testDescribePasswordLengthMedium() {
+    // Arrange
+    PasswordChecker mediumPassword = new PasswordChecker(6, 12);
+    // Act
+    String actualLength = mediumPassword.describePasswordLength("abc123abc");
+    // Assert
+    assertEquals("medium", actualLength);
   }
 
   @Test
