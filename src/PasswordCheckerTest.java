5aa7293a775dfd531dfbdb69434ca5ebf832df62
Juno Arciaga <junoarciaga@Junos-MacBook-Pro.local>
2024-10-08 15:14:59 -0700

    Created Tests

---
+++ src/PasswordCheckerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class PasswordCheckerTest {
+    @Test
+    void testDescribePasswordLength() {
+
+    }
+
+    @Test
+    void testIsAlphanumeric() {
+
+    }
+
+    @Test
+    void testIsBannedPassword() {
+
+    }
+}
