c13d196da48b90d5df2d39014713330ea7aa0bea
HumaGitGud <khomidov.huma@student.greenriver.edu>
2024-10-08 15:04:47 -0700

    added 2 more tests to the descrigePasswordLength

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
-    void testDescribePasswordLength() {
+    void testDescribePasswordLengthMedium() {
         // Arrange
         PasswordChecker password = new PasswordChecker(5, 10);
         // Act
@@ -12,11 +12,31 @@ public class PasswordCheckerTest {
         // Assert
         assertEquals("medium", actualLength);
     }
+    
+    @Test
+    void testDescribePasswordLengthShort() {
+        // Arrange
+        PasswordChecker password = new PasswordChecker(6, 6);
+        // Act
+        String actualLength = password.describePasswordLength("egg");
+        // Assert
+        assertEquals("short", actualLength);
+    }
+    
+    @Test
+    void testDescribePasswordLengthLong() {
+        // Arrange
+        PasswordChecker password = new PasswordChecker(12, 20);
+        // Act
+        String actualLength = password.describePasswordLength("ToyotaSupraCoolestCarTurbo");
+        // Assert
+        assertEquals("long", actualLength);
+    }
 
     @Test
     void testIsAlphanumeric() {
         // Arrange
-
+        
         // Act
 
         // Assert
