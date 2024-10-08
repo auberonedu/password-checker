bacded1bedc8a36790f9b6a3b8215cb86ede940a
Kimberly Mageary <kimberlymageary@gmail.com>
2024-10-08 15:00:04 -0700

    Added test case for short password

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
-    void testDescribePasswordLength() {
+    void testDescribePasswordLengthMedium() {
         // Assign
         PasswordChecker checker = new PasswordChecker(4, 6);
         // Act
@@ -12,6 +12,15 @@ public class PasswordCheckerTest {
         // Assert
         assertEquals("medium", actualLength);
     }   
+    @Test
+    void testDescribePasswordLengthShort(){
+        // Assign
+        PasswordChecker checker = new PasswordChecker(4, 6);
+        // Act
+        String actualLength= checker.describePasswordLength("kma");
+        // Assert
+        assertEquals("short", actualLength);
+    }
 
     @Test
     void testIsAlphanumeric() {
