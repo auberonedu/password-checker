e13328083a73a1fb71c8f9664b3a3d6f3b83dd5c
Madeleine Chance <mechance782@gmail.com>
2024-10-08 14:57:08 -0700

    describePasswordLength test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,10 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
     void testDescribePasswordLength() {
-
-    }
+        // Assign
+        PasswordChecker checker = new PasswordChecker(4, 6);
+        // Act
+        String actualLength = checker.describePasswordLength("ababa");
+        // Assert
+        assertEquals("medium", actualLength);
+    }   
 
     @Test
     void testIsAlphanumeric() {
