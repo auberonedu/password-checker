f809ad79658cb74a6708b335dbeea15eb0c589c2
Augy Markham <katiefaithmarkham@gmail.com>
2024-10-08 14:59:20 -0700

    added first test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,9 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
-    void testDescribePasswordLength() {
-
+    void testDescribePasswordLengthShort() {
+        // Arrange
+        PasswordChecker checker = new PasswordChecker(2, 7);
+        // Act
+        String actual = checker.describePasswordLength("a");
+        // Assert
+        assertEquals("short", actual);
     }
 
     @Test
