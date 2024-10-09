288cd5bb02925b6fed63fc87bcbcae7c0f003dbf
LibanBritt <liban3367@gmail.com>
2024-10-08 17:55:23 -0700

    Finished 6 test ensuring each banned password works per method

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
@@ -23,30 +25,72 @@ public class PasswordCheckerTest {
     @Test
     void testIsBannedPasswordpassword123() {
 
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("password123");
+        //Assertion
+        assertEquals(true, passwordTF);
+
     }
 
     @Test
     void testIsBannedPassword123456() {
+
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("123456");
+        //Assertion
+        assertEquals(true, passwordTF);
 
     }
 
     @Test
     void testIsBannedPasswordqwerty() {
 
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("qwerty");
+        //Assertion
+        assertEquals(true, passwordTF);
+
     }
 
     @Test
     void testIsBannedPasswordletmein() {
+
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("letmein");
+        //Assertion
+        assertEquals(true, passwordTF);
 
     }
 
     @Test
     void testIsBannedPasswordpassword() {
 
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("password");
+        //Assertion
+        assertEquals(true, passwordTF);
+
     }
 
     @Test
     void testIsBannedPasswordhello() {
+
+        //Arrange
+        PasswordChecker banned = new PasswordChecker(6, 12);
+        //Action
+        boolean passwordTF = banned.isBannedPassword("hello");
+        //Assertion
+        assertEquals(true, passwordTF);
 
     }
     
