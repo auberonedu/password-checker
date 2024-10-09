86207544465f0e9a56f636738cd9e2e9487326ef
Tavparsad Singh <tavparsadsingh2@gmail.com>
2024-10-09 11:52:38 -0700

    First successful test.

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,10 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
     void testDescribePasswordLength() {
+        //Arrange
         PasswordChecker checker = new PasswordChecker(5, 8);
+        //Act
         String length = checker.describePasswordLength("James");
+        //Assert
+        assertEquals("medium", length);
     }
 
     @Test
