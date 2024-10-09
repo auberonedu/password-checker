5e3662853f8f8a20613033e512f460a28d3728f7
Tavparsad Singh <tavparsadsingh2@gmail.com>
2024-10-09 11:47:39 -0700

    Made changes to the PasswordCheckerTest

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -3,7 +3,8 @@ import org.junit.jupiter.api.Test;
 public class PasswordCheckerTest {
     @Test
     void testDescribePasswordLength() {
-
+        PasswordChecker checker = new PasswordChecker(5, 8);
+        String length = checker.describePasswordLength("James");
     }
 
     @Test
