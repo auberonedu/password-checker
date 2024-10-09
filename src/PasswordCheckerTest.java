bac24e3951aa3836a4c2297aacb6f3236f309b2a
Diana <Khachaturova.Diana@student.greenriver.edu>
2024-10-09 12:58:54 -0700

    Got JUnit tests to work and added a second test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -3,16 +3,30 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
+    // TEST 1
     @Test
-    void testDescribePasswordLengthOne() {
-        //Arrange
+    void testMediumPasswordLength() {
+        // Arrange
         PasswordChecker checker = new PasswordChecker(5, 6);
         
-        //Act 
+        // Act 
         String describePasswordLength = checker.describePasswordLength("hello");
         
-        //Assert 
+        // Assert 
         assertEquals("medium", describePasswordLength);
+    }
+
+    // TEST 2
+    @Test 
+    void testLongPasswordLength() {
+        // Arrange
+        PasswordChecker checker = new PasswordChecker(5, 10);
+        
+        // Act 
+        String describePasswordLength = checker.describePasswordLength("superlongrandompassword");
+        
+        // Assert 
+        assertEquals("long", describePasswordLength);
     }
 
     @Test
