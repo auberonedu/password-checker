6ab0b27a715bd0d03e5093c9ce302b46c6c52f37
RMarx1456 <hellohi12465@gmail.com>
2024-10-11 12:21:51 -0700

    Finished testcases for testDescribePasswordLength.

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -8,6 +8,10 @@ public class PasswordCheckerTest {
         PasswordChecker checkLength = new PasswordChecker(6, 10);
         checkLength.describePasswordLength("password");
         assertEquals("medium", checkLength.describePasswordLength("password"));
+        checkLength.describePasswordLength("SuperMarioWowie!");
+        assertEquals("long", checkLength.describePasswordLength("SuperMarioWowie!"));
+        checkLength.describePasswordLength("Hi!");
+        assertEquals("short", checkLength.describePasswordLength("Hi!"));
     }
 
     @Test
