bf0f92fdf7e3b633f69db78a6302297d9e46e71c
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-11 12:31:13 -0700

    Added a testIsAlphaNumeric() method and encounter a bug on the PasswordChecker.java class, pushing for Raymond to work on the bug.

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -7,16 +7,22 @@ public class PasswordCheckerTest {
     void testDescribePasswordLength() {
         PasswordChecker checkLength = new PasswordChecker(6, 10);
         checkLength.describePasswordLength("password");
-        assertEquals("medium", checkLength.describePasswordLength("password"));
         checkLength.describePasswordLength("SuperMarioWowie!");
-        assertEquals("long", checkLength.describePasswordLength("SuperMarioWowie!"));
         checkLength.describePasswordLength("Hi!");
+        assertEquals("medium", checkLength.describePasswordLength("password"));
+        assertEquals("long", checkLength.describePasswordLength("SuperMarioWowie!"));
         assertEquals("short", checkLength.describePasswordLength("Hi!"));
     }
 
     @Test
     void testIsAlphanumeric() {
-
+        PasswordChecker checkAlphaNum = new PasswordChecker(6, 10);
+        checkAlphaNum.isAlphanumeric("password");
+        checkAlphaNum.isAlphanumeric("SuperMarioWowie!");
+        checkAlphaNum.isAlphanumeric("Hi!");
+        assertEquals(true, checkAlphaNum.isAlphanumeric("password"));
+        assertEquals(false, checkAlphaNum.isAlphanumeric("SuperMarioWowie!"));
+        assertEquals(false, checkAlphaNum.isAlphanumeric("Hi!"));
     }
 
     @Test
