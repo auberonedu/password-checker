e1b6dafe3635bad8e519e4261aa8703b28c8d765
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-11 12:18:54 -0700

    Created a Test Class and added the method testDescribePasswordLength()

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,9 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
     @Test
     void testDescribePasswordLength() {
-
+        PasswordChecker checkLength = new PasswordChecker(6, 10);
+        checkLength.describePasswordLength("password");
+        assertEquals("medium", checkLength.describePasswordLength("password"));
     }
 
     @Test
